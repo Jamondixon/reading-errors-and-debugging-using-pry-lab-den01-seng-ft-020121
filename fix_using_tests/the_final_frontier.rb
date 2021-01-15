3345fd1f4630c1b27b97ec1d3ec14bc21de91e6a
Jamon Dixon <jamonadixon@gmail.com>
2021-01-15 00:38:40 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,7 +2,7 @@
 require 'pry'
 
 def generate_star_date
-  (sum(100000) + 400000) / 10.0
+  (rand(100000) + 400000) / 10.0
 end
 
 
@@ -14,7 +14,7 @@ end
 
 def engage
   date = generate_star_date
-  
+
   puts state_log(date)
 end
 engage
