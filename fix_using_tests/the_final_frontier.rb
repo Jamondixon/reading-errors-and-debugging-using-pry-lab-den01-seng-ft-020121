d714dabc3eb3cd73d7b0b82848d3944e8cf30552
Jamon Dixon <jamonadixon@gmail.com>
2021-01-15 00:19:12 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,6 +1,6 @@
 # don't forget to add: require 'pry'
 
-def generate_star_date
+def star_date
   (rand(100000) + 400000) / 10.0
 end
 
@@ -9,7 +9,10 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
+
+
 def engage
-  puts state_log(date)
-  date = generate_star_date
+  p state_log(date)
+  p date = star_date
+
 end
